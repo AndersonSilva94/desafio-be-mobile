40cdb48a4017229f1fe72240b8b2a52429227cd6
Anderson Silva <andssilva.23@gmail.com>
2021-11-01 22:38:04 -0400

    feat: :rocket: salva o tema no local storage

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Header from './components/Header';
 import dark from './themes/dark';
 import light from './themes/light';
 import { ThemeProvider } from 'styled-components';
+import useLocalStorageTheme from './utils/useLocalStorageTheme';
 
 function App() {
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = useLocalStorageTheme('theme', light);
 
   const toggleTheme = () => {
     setTheme(theme.title === 'light' ? dark : light)
